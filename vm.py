f39ce595473b98778f3319a035ce3297c583a180
Anthony Landreth <robophilosopher@gmail.com>
2018-02-11 20:31:48 -0800

    starts building a runtime

---
+++ vm.py
@@ -0,0 +1,89 @@
+import pdb
+
+class ProgramObject():
+    def __init__(self):
+        self._code = []
+        self._const = []
+
+    def code(self, ins):
+        self._code.append(ins)
+
+    def const(self, val):
+        self._const.append(val)
+
+class VM():
+
+    def run(po):
+        ip = 0
+        tmp = {}
+
+        code = []
+        store = {}
+
+        code.append(po.code)
+
+        constant_index = 1
+
+        for c in po._const:
+            store['c{0}'.format(constant_index)] = c
+            constant_index += 1
+
+
+        while(True):
+            try:
+                ins = po._code[ip].split(" ")
+            except:
+                break
+            ip += 1
+
+            op = ins[0]
+
+            if op == 'set':
+                tmp[ins[1]] = ins[2]
+
+            if op == 'iadd':
+                tmp[ins[1]] = int(tmp[ins[2]]) + int(tmp[ins[3]])
+
+            if op == 'isub':
+                tmp[ins[1]] = int(tmp[ins[2]]) - int(tmp[ins[3]])
+
+            if op == 'cload':
+                tmp[ins[1]] = store[ins[2]]
+
+            if op == 'print':
+                # result is for testing purposes
+                result = tmp[ins[1]]
+
+                print(result)
+                return result
+
+po = ProgramObject()
+po.code("set t1 1")
+po.code("set t2 2")
+po.code("iadd t3 t1 t2")
+po.code("print t3")
+assert(VM.run(po)) == 3
+
+po = ProgramObject()
+po.code("set t1 2")
+po.code("set t3 3")
+po.code("isub t4 t3 t1")
+po.code("set t5 1")
+po.code("isub t2 t4 t5")
+po.code("print t2")
+assert(VM.run(po)) == 0
+
+po = ProgramObject()
+po.code("set t1 2")
+po.code("set t3 3")
+po.code("set t4 1")
+po.code("isub t5 t1 t4")
+po.code("isub t2 t3 t5")
+po.code("print t2")
+assert(VM.run(po)) == 2
+
+po = ProgramObject()
+po.const('hello world')
+po.code('cload t1 c1')
+po.code('print t1')
+assert(VM.run(po)) == 'hello world'
